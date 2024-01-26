8944851894959572531a0a1228dcfdd88004df17
Shinya Ishizaki <shinya.ishizaki.0311@gmail.com>
2024-01-26 12:52:34 +0900

    recoding how to create password

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 function createPassword() {
-  console.log("*** btn clicked ***");
-  
+  console.log("*** createPassword START ***");
+
+  // パスワードに使用する文字列を定義する
+  var bigAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var smallAlphabets = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var signs = "-=^~\||@`[{;+:*]},<.>/?_"
+
+  // 英大文字からランダムに1文字取り出す
+  var randomBigAlphabet = bigAlphabets[Math.romdom()];
+
+  // 英小文字からランダムに1文字取り出す
+  var randomSmallAlphabet = smallAlphabets[Math.romdom()];
+
+  // 数字からランダムに1文字取り出す
+  var randomNumber = numbers[Math.romdom()];
+
+  // 記号からランダムに1文字取り出す
+  var randomSign = signs[Math.romdom()];
+
   // パスワードを生成する
-  var password = Math.random().toString(36).slice(-8);
+  var password = randomBigAlphabet + randomSmallAlphabet + randomNumber + randomSign;
 
-  console.log(password);
+  console.log("CreatedPassword: " + password);
 
-  // パスワードをテキストボックスに設定する
   document.getElementById("textbox").value = password;
+
+  console.log("*** createPassword END ***");
 }
 
 
